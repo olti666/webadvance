40b718206df0d19921eedf8304eefb8afa2a1ea5
olti666 <oltihasani0@gmail.com>
2026-03-05 17:41:29 +0100

    moduli26

---
+++ moduli26/main.js
@@ -0,0 +1,27 @@
+console.log("Hellooo");
+
+var x=10;
+var y=30;
+console.log(x+y);
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
+
+var a=99;
+
+a++;
+
+console.log(a);
+
+var b=101;
+
+b--;
+
+console.log(b);
+
+var c=500;
+c+=5
+console.log(c)
+var d=500;
+d-=5
+console.log(d)
