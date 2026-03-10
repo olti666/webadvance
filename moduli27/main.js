7b70df2f465a8bed8bfcdc5d85dac8365afdd8fa
olti666 <oltihasani0@gmail.com>
2026-03-10 17:43:01 +0100

    moduli27

---
+++ moduli27/main.js
@@ -0,0 +1,9 @@
+function displayAlert(){
+    alert("This text is red")
+}
+displayAlert();
+
+function writeHTML(number1 ,number2) {
+    return number1+number2;
+}
+document.write(writeHTML(8, 3))
