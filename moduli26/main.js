15e266c4ca1d2a80ea99c20b815c4f5e325515e0
olti666 <oltihasani0@gmail.com>
2026-03-10 17:19:55 +0100

    moduli26

--- moduli26/main.js
+++ moduli26/main.js
@@ -25,3 +25,19 @@ console.log(c)
 var d=500;
 d-=5
 console.log(d)
+
+var name = "Olti";
+var surname = "Hasani";
+var full_name = name +" "+ surname
+console.log(full_name);
+
+var age =15
+
+console.log(age <= 18 && age <= 65);
+var age1 = 15;
+console.log(age1 >= 18 || age1>=10);
+
+var one = 16;
+var two = 16;
+
+console.log(!(one == two));
