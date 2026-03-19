5a7687416bbe29b455c92a9b623fe4a45a98b348
olti666 <oltihasani0@gmail.com>
2026-03-19 17:46:25 +0100

    Update main.js

--- moduli28/main.js
+++ moduli28/main.js
@@ -1,5 +1,17 @@
-var button2 = document.querySelector('button');
+var button1 = document.getElementById("btn1");
+var tekst1 = document.getElementById("txt1");
 
-button2.onclick = function(){
-    alert('Button clicked!');
+
+button1.onclick = function() {
+    tekst.style.color = "red";
+    tekst.style.backgroundColor = "yellow";
+    tekst.style.fontSize = "100px";
+    tekst.style.padding = "20px";
+    teksti.style.textAlign = "center";
 }
+
+button2.onclick = function() {
+    tekst.style.cssText = "color: red; background-color: yellow; font-size: 100px; padding: 20px; text-align: center;";}
+
+    var button2 = document.getElementById("i fundit");
+    
