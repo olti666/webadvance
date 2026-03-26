85dc837b4e7b5bd99698afd0a009188f20d03627
olti666 <oltihasani0@gmail.com>
2026-03-26 17:18:43 +0100

    moduli29

--- moduli29/main.js
+++ moduli29/main.js
@@ -1,4 +1,4 @@
-var propgramming = ["JavaScript", "PYTHON", "JAVA",];
+var propgrammingLanguages = ["JavaScript", "PYTHON", "JAVA",];
 
 console.log(propgrammingLanguages);
 
@@ -6,4 +6,62 @@ var numrat = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(numrat);
 
-consol
+console.log(propgrammingLanguages[1]);
+
+propgrammingLanguages.push("Java");
+
+numrat.push(11);
+
+document.write(propgrammingLanguages);
+
+document.write(numrat);
+
+numrat.pop();
+
+console.log(numrat);
+
+propgrammingLanguages.unshift("C#");
+
+console.log(propgrammingLanguages);
+
+propgrammingLanguages.shift();
+
+console.log(propgrammingLanguages);
+
+propgrammingLanguages.splice(1, 0, "Ruby");
+
+console.log(propgrammingLanguages);
+
+var students = ["Alice", "Bob", "Charlie", "David"];
+
+var [s1, s2, s3, s4] = students;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+var citys = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Austin", "Jacksonville", "Fort Worth", "Columbus", "Charlotte", "San Francisco", "Indianapolis", "Seattle", "Denver", "Washington, D.C.", "Boston"];
+
+    var [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20] = citys;
+
+console.log(c1);
+console.log(c2);
+console.log(c3);
+console.log(c4);
+console.log(c5);
+console.log(c6);
+console.log(c7);
+console.log(c8);
+console.log(c9);
+console.log(c10);
+console.log(c11);
+console.log(c12);
+console.log(c13);
+console.log(c14);
+console.log(c15);
+console.log(c16);
+console.log(c17);
+console.log(c18);
+console.log(c19);
+console.log(c20);
