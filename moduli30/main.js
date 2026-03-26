504d77ed3975020cc5338cb7850119e923860203
olti666 <oltihasani0@gmail.com>
2026-03-26 17:43:36 +0100

    moduli30

---
+++ moduli30/main.js
@@ -0,0 +1,18 @@
+var mosha = 10;
+
+if(mosha < 8){
+    document.write("Fmija juaj eshte i vogel qe te filloj shkollen e programimit");
+}else {
+    document.write("Fmija juaj eshte i madh qe te filloj shkollen e programimit");
+}
+
+var age = 20;
+
+if(mosha < 8){
+    document.write("Fmija juaj eshte i vogel qe te filloj shkollen e programimit");
+
+else if(age < 18){
+    document.write("Ju nuk jeni i rritur qe te filloj shkollen e programimit");
+}else {
+    document.write("Ju jeni i rritur qe te filloj shkollen e programimit");
+}
