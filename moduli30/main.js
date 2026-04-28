4d2fe60dfbbb002879f75dfc67e462760976dc9b
olti666 <oltihasani0@gmail.com>
2026-04-28 17:42:45 +0200

    moduli33

--- moduli30/main.js
+++ moduli30/main.js
@@ -16,3 +16,28 @@ else if(age < 18){
 }else {
     document.write("Ju jeni i rritur qe te filloj shkollen e programimit");
 }
+var input = document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text = document.getElementById('text_id');
+
+button.onclick = function (){
+    text.innerHTML = input.value;
+}
+
+var num1 = document.getElementById('input_id1');
+var num2 = document.getElementById('input_id2');
+var btn = document.getElementById('btn2_id');
+var result = document.getElementById('result_id');
+
+btn.onclick = function() {
+    result.innerHTML = num1.value + num2.value;
+    if(result > 10){
+        text2.innerHTML = "greater then 10"
+    }
+    else if(result2 < 10){
+        text2.inner.HTML ="less than 10"
+    }
+    else {
+        
+    }
+}
